fcc2bf8403e0428807d56203815608f2cd5b1674
tinbob655 <wrutland07@gmail.com>
2026-08-07 14:28:32 +0100

    added random move selector as a fallback for testing

---
+++ server/src/main/java/com/hive/server/service/bot/RandomBot.java
@@ -0,0 +1,21 @@
+package com.hive.server.service.bot;
+
+import com.hive.server.model.move.Move;
+import com.hive.server.model.state.GameState;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Fallback;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@Fallback
+@Qualifier("random")
+public final class RandomBot implements Bot {
+
+    @Override
+    public Move decideMove(GameState state) {
+        Set<Move> legalMoves = state.legalMoves();
+        return legalMoves.stream().findAny().orElseThrow();
+    }
+}
