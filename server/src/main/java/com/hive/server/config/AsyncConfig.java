d5f102d18676db8af6d3e684d11c19e4ad3dbcad
tinbob655 <wrutland07@gmail.com>
2026-08-08 10:38:15 +0100

    created executor for minimax bot

---
+++ server/src/main/java/com/hive/server/config/AsyncConfig.java
@@ -0,0 +1,31 @@
+package com.hive.server.config;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    @Bean(name = "taskExecutor")
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(4);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("hive-async-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (throwable, method, params) ->
+                System.err.println("Uncaught async error in " + method.getName() + ": " + throwable.getMessage());
+    }
+}
